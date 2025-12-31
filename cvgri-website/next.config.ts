4da210f3f894fb3c6076e86a2dbe96e5787e8cec
JoshuaTadesseMek <joshuatadesse314@gmail.com>
2026-01-01 00:32:33 +0300

    commit

--- cvgri-website/next.config.ts
+++ cvgri-website/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "cvgri.github.io";
+
 const nextConfig: NextConfig = {
   output: "export",
-
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true, // REQUIRED for GitHub Pages
   },
